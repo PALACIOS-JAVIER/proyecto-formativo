59ec5f7fecccaaad854145aad76c307a05a0dea1
Arcos2819 <arcosmunozemiliano719@gmail.com>
2026-06-03 10:55:09 -0500

    Servive y module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,4 +1,6 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { UsuarioModule } from './usuario/usuario.module';
 import { AreaModule } from './area/area.module';
 import { RolModule } from './rol/rol.module';
@@ -13,8 +15,30 @@ import { ActividadModule } from './actividad/actividad.module';
 import { EvidenciaModule } from './evidencia/evidencia.module';
 
 @Module({
-  imports: [UsuarioModule, AreaModule, RolModule, NovedadModule, TipoContratroModule, ObligacionesModule, ControlCalidadModule, InformeModule, InformeGfModule, InformeGcModule, ActividadModule, EvidenciaModule],
-  controllers: [],
-  providers: [ ],
+  imports: [
+    ConfigModule.forRoot(),
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: process.env.DB_HOST,
+      port: parseInt(process.env.DB_PORT ?? '5432'),
+      username: process.env.DB_USERNAME,
+      password: process.env.DB_PASSWORD,
+      database: process.env.DB_NAME,
+      autoLoadEntities: true,
+      synchronize: true,
+    }),
+    UsuarioModule,
+    AreaModule,
+    RolModule,
+    NovedadModule,
+    TipoContratroModule,
+    ObligacionesModule,
+    ControlCalidadModule,
+    InformeModule,
+    InformeGfModule,
+    InformeGcModule,
+    ActividadModule,
+    EvidenciaModule,
+  ],
 })
 export class AppModule {}
--- src/usuario/usuario.service.ts
+++ src/usuario/usuario.service.ts
@@ -1,11 +1,43 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, InternalServerErrorException, Logger, BadRequestException } from '@nestjs/common';
 import { CreateUsuarioDto } from './dto/create-usuario.dto';
 import { UpdateUsuarioDto } from './dto/update-usuario.dto';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+import { Usuario } from './entities/usuario.entity';
 
 @Injectable()
 export class UsuarioService {
-  create(createUsuarioDto: CreateUsuarioDto) {
-    return 'This action adds a new usuario';
+
+  private readonly logger = new Logger('UsuarioService');
+
+  constructor(
+    @InjectRepository(Usuario)
+    private readonly usuarioRepository: Repository<Usuario>,
+  ) {}
+
+  async create(createUsuarioDto: CreateUsuarioDto) {
+    try {
+      const {
+        fk_codigo_contrato,
+        fk_area,
+        fk_rol,
+        fk_novedad,
+        ...rest
+      } = createUsuarioDto;
+
+      const usuario = this.usuarioRepository.create({
+        ...rest,
+        fk_codigo_contrato: { id: fk_codigo_contrato },
+        area: { id: fk_area },
+        rol: { id: fk_rol },
+        novedad: { id: fk_novedad },
+      });
+
+      await this.usuarioRepository.save(usuario);
+      return usuario;
+    } catch (error) {
+      this.handleDBExceptions(error);
+    }
   }
 
   findAll() {
@@ -22,5 +54,14 @@ export class UsuarioService {
 
   remove(id: number) {
     return `This action removes a #${id} usuario`;
+  }
+
+    private handleDBExceptions(error: any){
+
+          if ((error as any).code === '23505') {
+        throw new BadRequestException((error as any).detail);
+      }
+      this.logger.error(error)
+      throw new InternalServerErrorException('Error desconosido, revise el log del servidor.');
   }
 }
